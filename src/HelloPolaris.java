50a7f5d3f95c5fb6c07959af6c0d73a2ac4275a2
deceir <83772817+deceir@users.noreply.github.com>
2021-07-29 15:31:23 -0500

    completed second exercise

--- src/HelloPolaris.java
+++ src/HelloPolaris.java
@@ -1,7 +1,49 @@
 public class HelloPolaris {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // This is the console.log equivalent
         System.out.println("Hello!");
         System.out.println("Polaris!");
+
+        //Exercise part 1
+        int myFavoriteNumber = 4;
+        System.out.println(myFavoriteNumber);
+
+        // Exercise part 2
+        String myString = "This is a string and it contains content.";
+        System.out.println(myString);
+
+        // Calling for this with no value encounters a compile error
+        double myNumber = 123;
+//
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        // Reserved words cannot be used
+//        String class = "Hello, World!";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//
+//        int x = 4;
+//        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        System.out.println(y);
+
+
+
     }
 }
