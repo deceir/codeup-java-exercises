f3493a76dec1ed5ad1b6bc503b515b11eb9a8d2a
deceir <83772817+deceir@users.noreply.github.com>
2021-08-02 14:15:20 -0500

    completed area/perimeter exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,28 +3,44 @@ import java.util.Scanner;
 public class ConsoleExercises {
 
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
-        double pi = 3.14159;
-
-        System.out.printf("The value of pi is approximately equal to %s.", pi);
-
+//
+//        double pi = 3.14159;
+//
+//        System.out.printf("The value of pi is approximately equal to %s.", pi);
+//
 //        int userInteger = scanner.nextInt();
 //
 //        System.out.println("The int you entered is " + userInteger);
-//
+
 //        String userInput1 = scanner.next();
 //        String userInput2 = scanner.next();
 //        String userInput3 = scanner.next();
-//
+
 //        System.out.println(userInput1);
 //        System.out.println(userInput2);
 //        System.out.println(userInput3);
-//
+
 //        String userLine = scanner.nextLine();
 //        System.out.println(userLine);
+
+        System.out.println("We will need a length and a width to continue!");
+        System.out.println("Give us a width: ");
+
+        double width = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Give us a length: ");
+
+        double length = Double.parseDouble(scanner.nextLine());
+
+        double area = length * width;
+        double perimeter = (width * 2) + (length * 2);
+
+        System.out.println("Area is: " + area);
+        System.out.println("Perimeter is: " + perimeter);
+
 
     }
 
