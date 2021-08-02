a07c2ba63aeef2ed28243a67b589dea30377b627
deceir <83772817+deceir@users.noreply.github.com>
2021-08-02 16:30:44 -0500

    loop table complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -45,19 +45,36 @@ public class ControlFlowExercises {
 
         //FizzBuzz
 
+//        for (int index = 0; index <= 100; index++) {
+//            if (index % 3 == 0 && index % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (index % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (index % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(index);
+//            }
+//        }
 
-        for (int index = 0; index <= 100; index++) {
-//            System.out.println(index);
-            if (index % 3 == 0 && index % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (index % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (index % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(index);
-            }
-        }
+//        //Table Loop
+//
+//        System.out.println("What number do you choose?");
+//        int userInteger = scanner.nextInt();
+//        System.out.println("Here is your table!");
+//        System.out.print(" number ");
+//        System.out.print('|');
+//        System.out.print(" squared ");
+//        System.out.print('|');
+//        System.out.print(" cubed ");
+//        System.out.printf("%n");
+//        System.out.println("______  |  ______ | _____");
+//
+//        for (int loopIndex = 1; loopIndex <= userInteger; loopIndex++) {
+//            System.out.println(loopIndex + "       | " + (loopIndex * loopIndex) + "       | " + (loopIndex * loopIndex * loopIndex));
+//        }
+
+
 
     }
 
