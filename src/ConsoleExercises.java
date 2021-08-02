ec101ed48e60b4a13d3b910b44d564e21ab5dc56
deceir <83772817+deceir@users.noreply.github.com>
2021-08-02 11:47:14 -0500

    completed initial scanner exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,10 +1,29 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class ConsoleExercises {
-    public static void main(String[] args) {
+
+
+    public void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         double pi = 3.14159;
 
         System.out.printf("The value of pi is approximately equal to %s.", pi);
+
+        int userInteger = scanner.nextInt();
+
+        System.out.println("The int you entered is " + userInteger);
+
+        String userInput1 = scanner.next();
+        String userInput2 = scanner.next();
+        String userInput3 = scanner.next();
+
+        System.out.println(userInput1);
+        System.out.println(userInput2);
+        System.out.println(userInput3);
+
     }
+
+
 }
