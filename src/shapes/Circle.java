27f30be0c67be04fa0e3e9b3a4410088749d47af
deceir <83772817+deceir@users.noreply.github.com>
2021-08-05 16:22:34 -0500

    added circle file

---
+++ src/shapes/Circle.java
@@ -0,0 +1,30 @@
+package shapes;
+
+import util.Input;
+
+public class Circle {
+
+    private double radius;
+
+    public double getArea() {
+
+    }
+
+
+    public double getCircumference() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    // Accessors
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
