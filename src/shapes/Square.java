84bf8f136881ac4b672eb3ccfe2b8935d367a7bc
deceir <83772817+deceir@users.noreply.github.com>
2021-08-09 15:35:12 -0500

    exercise beginning

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,20 +2,20 @@ package shapes;
 
 public class Square extends Rectangle {
 
-    protected int side;
-
-    @Override
-    public int getArea() {
-        return this.length * this.length;
-    }
-
-    @Override
-    public int getPerimeter() {
-        return this.length * 4;
-    }
-
-    public Square(int side) {
-        super(side, side);
-    }
+//    protected int side;
+//
+//    @Override
+//    public int getArea() {
+//        return this.length * this.length;
+//    }
+//
+//    @Override
+//    public int getPerimeter() {
+//        return this.length * 4;
+//    }
+//
+//    public Square(int side) {
+//        super(side, side);
+//    }
 
 }
