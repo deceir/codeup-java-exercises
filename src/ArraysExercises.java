87e8c9242a86f13316b6ef47147e8702cee6fe63
deceir <83772817+deceir@users.noreply.github.com>
2021-08-10 11:56:49 -0500

    exercise one completed arrays

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,10 +1,26 @@
+import java.util.Arrays;
 public class ArraysExercises {
 
+    public static Person[] addPerson(Person[] array, Person person) {
+
+        Person[] newPerson = Arrays.copyOf(array, array.length + 1);
+        newPerson[newPerson.length - 1] = person;
+        return newPerson;
+    }
 
     public static void main(String[] args) {
 
+        Person[] arrayOfPeople = {new Person("Jon"), new Person("Ron"), new Person("Bob")};
 
+        for (Person person : arrayOfPeople) {
+            System.out.println(person.getName());
+        }
+
+
+
+        for (Person person : addPerson(arrayOfPeople, new Person("Ronny"))) {
+            System.out.println(person.getName());
+        }
 
     }
-
 }
